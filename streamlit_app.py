a47fc506c3ee593ab7b8a29dc74138427e59bcc1
ambikabe <160489119+ambikabe@users.noreply.github.com>
2024-02-27 20:03:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,13 +68,13 @@ try:
 # streamlit.header("The fruit load list contains:")
 # streamlit.dataframe(my_data_row)
 
-# my_cur.execute("select * from fruit_load_list")
-# my_data_rows=my_cur.fetchall()
-# streamlit.header("The fruit load list contains:")
-# streamlit.dataframe(my_data_rows)
+my_cur.execute("select * from fruit_load_list")
+my_data_rows=my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
-# fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-# streamlit.write('Thanks for adding ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
 
-# my_cur.execute("insert into fruit_load_list values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
