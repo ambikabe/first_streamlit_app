b88ff9bb125d937f04c4e3519f618cb505bc1c9b
ambikabe <160489119+ambikabe@users.noreply.github.com>
2024-02-27 13:25:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
-import requests;
-import snowflake.connector;
+import requests
+import snowflake.connector
 streamlit.title('my parents new healthy diner')
 streamlit.header(' Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
