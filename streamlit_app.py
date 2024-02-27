2af3401a82b15d6ac59b780de1c910b0f6b82c5b
ambikabe <160489119+ambikabe@users.noreply.github.com>
2024-02-27 20:16:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
     #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     #streamlit.dataframe(fruityvice_normalized)
 
-#except URLError as e:
+except URLError as e:
   
 def get_fruityvice_data(this_fruit_choice):
  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
